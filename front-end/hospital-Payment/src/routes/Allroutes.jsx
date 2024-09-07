16d27041404e06397317d87585803f2401e1f890
StormRobert <stormarasa2@gmail.com>
2024-09-07 12:40:15 +0300

    Authentification pages

--- front-end/hospital-Payment/src/routes/Allroutes.jsx
+++ front-end/hospital-Payment/src/routes/Allroutes.jsx
@@ -1,6 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "../pages/Home"
 import Dashboard from '../pages/Dashboard';
+import Login from '../pages/Authentification/Login';
+import Signup from '../pages/Authentification/Signup';
+import UpdatePassword from '../pages/Authentification/Updatepassword';
+import VerifyPassword from '../pages/Authentification/Verify';
 
 const Allroutes = () => {
   return (
@@ -8,6 +12,10 @@ const Allroutes = () => {
       <Routes>
         <Route path="/" element={<Home />} />    
         <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/update-password" element={<UpdatePassword />} />
+      <Route path="/verify-password" element={<VerifyPassword />} />
       </Routes>
     </Router>
   )
